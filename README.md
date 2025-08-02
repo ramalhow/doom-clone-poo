e1212763289c7f77df86930a6a863aa7e6a7425c
Arthur Ramalho <arthu.ramalh0z@gmail.com>
2025-08-02 15:03:32 -0300

    mudando o sistema de build para cmake (infelizente)

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # doom-clone-poo
+
+## Instruções para compilar:
+
+No terminal linux (ou pelo WSL, caso esteja no Windows) instale as seguintes dependências:
+```
+sudo apt install build-essentials
+sudo apt install cmake
+```
+
+Por fim, execute os seguintes comandos para compilar o projeto.
+```
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build .
+```
--- src/Game.hpp
+++ src/Game.hpp
@@ -1,40 +1,41 @@
 #pragma once
+#include <iostream>
 #include "../engine/SpriteBuffer.hpp"
-#include "../engine/SpriteAnimado.hpp"
-#include "../engine/Cores.hpp"
+// #include "../engine/SpriteAnimado.hpp"
+// #include "../engine/Cores.hpp"
 
-class Game
-{
+class Game {
 public:
-	Game(){}
-	~Game(){}
-	
-	static void run()
-	{
-		SpriteBuffer buffer(250,57);
-	
-		/*
-		FaseStart start("FaseStart",SpriteAnimado("rsc/castleMedieval.anm",3,COR::VERMELHA));
-		FaseLevel1 fase1("Fase1",Sprite("rsc/fase2.img",COR::VERMELHA));
-		FaseFinal faseFinal("Fase1",Sprite("rsc/title1Gothic.img"));
-		
-		start.init();
-		start.run(buffer);
-		
-		buffer.clear();
-		fase1.init();
-		int ret1 = fase1.run(buffer);
-		if ( ret1 != Fase::GAME_OVER && ret1 != Fase::END_GAME){
-		
-			buffer.clear();
-			faseFinal.init();
-			faseFinal.run(buffer);
-		}
-		else
-			std::cout << "GAME OVER" << std::endl;
-		
-		std::cout << "Saindo..." << std::endl;
-		*/
-	}
+  Game() {}
+  ~Game() {}
 
+  static void run() {
+    SpriteBuffer buffer(250, 57);
+    std::cout << " world\n";
+    
+
+    /*
+    FaseStart
+    start("FaseStart",SpriteAnimado("rsc/castleMedieval.anm",3,COR::VERMELHA));
+    FaseLevel1 fase1("Fase1",Sprite("rsc/fase2.img",COR::VERMELHA));
+    FaseFinal faseFinal("Fase1",Sprite("rsc/title1Gothic.img"));
+
+    start.init();
+    start.run(buffer);
+
+    buffer.clear();
+    fase1.init();
+    int ret1 = fase1.run(buffer);
+    if ( ret1 != Fase::GAME_OVER && ret1 != Fase::END_GAME){
+
+            buffer.clear();
+            faseFinal.init();
+            faseFinal.run(buffer);
+    }
+    else
+            std::cout << "GAME OVER" << std::endl;
+
+    std::cout << "Saindo..." << std::endl;
+    */
+  }
 };
